9aa6561701d0914b16f5184d0c0d219eeff0df70
jkaplanis <johntkaplanis@gmail.com>
2020-06-11 13:37:46 -0700

    sample pdf rendered on page, download link success

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch
+  // Redirect
+} from "react-router-dom";
+import MyDocument from "./CreatePdf";
+import { PDFDownloadLink, PDFViewer } from "@react-pdf/renderer";
 
 function App() {
-  return <h1>Quote Generator</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <h1>Quote Generator</h1>
+        </Route>
+        <Route exact path="/preview">
+          <PDFViewer height="800" width="600">
+            <MyDocument />
+          </PDFViewer>
+        </Route>
+        <Route exact path="/pdf">
+          <PDFDownloadLink document={<MyDocument />} fileName="quote.pdf">
+            {({ blob, url, loading, error }) =>
+              loading ? "Loading document..." : "Download now!"
+            }
+          </PDFDownloadLink>
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
